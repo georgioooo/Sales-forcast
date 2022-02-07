e3dfb177ece7b5a643663f827bb23c8377fc32ad
georges sleimen <94161237+georgioooo@users.noreply.github.com>
2022-02-07 22:58:12 +0200

    Add files via upload

--- correlations_coef_tables_graphs.py
+++ correlations_coef_tables_graphs.py
@@ -19,23 +19,25 @@ correlation_itemB_table.to_excel("Table_B.xlsx")
 
 style.use("ggplot")
 
+# graph of Max Temperature and sales of item B
 pyplot.scatter(bordeaux_weather["MAX_TEMPERATURE_C"], history_itemB["SALES"])
 pyplot.xlabel("MAX_TEMPERATURE_C of bordeaux")
 pyplot.ylabel("SALES of item B")
 pyplot.show()
 
+# graph of Max Temperature and sales of item A
 pyplot.scatter(bordeaux_weather["MAX_TEMPERATURE_C"], history_itemA["SALES"])
 pyplot.xlabel("MAX_TEMPERATURE_C of bordeaux")
 pyplot.ylabel("SALES of item A")
 pyplot.show()
 
-
+# graph of Wind speed and sales of item B
 pyplot.scatter(lyon_weather["WINDSPEED_MAX_KMH"], history_itemB["SALES"])
 pyplot.xlabel("WINDSPEED_MAX_KMH of Lyon")
 pyplot.ylabel("SALES of item B")
 pyplot.show()
 
-
+# graph of cloud cover average percent and sales of item A
 pyplot.scatter(marseille_weather["CLOUDCOVER_AVG_PERCENT"], history_itemA["SALES"])
 pyplot.xlabel("CLOUDCOVER_AVG_PERCENT of marseille")
 pyplot.ylabel("SALES of item A")
